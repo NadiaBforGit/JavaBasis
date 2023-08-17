26e6a0d03d6e83e4768d46169209b06faf08326d
Nadezhda Burtseva <fraunadia2017@yandex.ru>
2023-08-17 14:11:52 +0300

    bit2

--- OperBit3_3/src/OperBit3_3.java
+++ OperBit3_3/src/OperBit3_3.java
@@ -3,13 +3,14 @@ public class OperBit3_3 {
 
    public static void main(String[] args) {
       int x=0b1011001;
+      System.out.println(Integer.toBinaryString(x));
       int n=0; //byte index
       while (x!=0) { //status check of last byte
          if ((x&1)!=0) {
-            System.out.println(n+"flag on");
+            System.out.println(n+" -flag on");
          }
          else {
-            System.out.println(n+"flag off");
+            System.out.println(n+" -flag off");
          }
          x=x>>>1; //move to right 1 position
          n++;
