956bfa401a7046ad1dcf7b5c469365ecb79006c0
Nadezhda Burtseva <fraunadia2017@yandex.ru>
2023-08-18 10:56:35 +0300

    operIF1

---
+++ OperIF3_4/src/OperIF3_4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+// check odd-even number
+public class OperIF3_4 {
+
+   public static void main(String[] args) {
+      Scanner in= new Scanner(System.in);
+      int x=0;
+      System.out.println("enter Integer");
+      x=in.nextInt();
+      if (x%2==0) {
+         System.out.println(x+" even number");
+      }
+      else {
+         System.out.println(x+" odd number");         
+      }
+
+   }
+
+}
