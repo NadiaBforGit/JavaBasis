2b71fee540a22cba32372584e0feb928aa6bf083
Nadezhda Burtseva <fraunadia2017@yandex.ru>
2023-09-04 10:44:50 +0300

    +arr code

--- OperIF3_4/src/OperIF3_4.java
+++ OperIF3_4/src/OperIF3_4.java
@@ -1,21 +1,31 @@
 import java.util.Scanner;
+
 // check odd-even number
 public class OperIF3_4 {
 
    public static void main(String[] args) {
       Scanner in= new Scanner(System.in);
       int x=0;
-      System.out.println("enter Integer");
+      System.out.print("enter Integer ");
       x=in.nextInt();
       if (x%2==0) {
          System.out.println(x+" even number");
       }
       else {
          System.out.println(x+" odd number");         
-      }
+      }        
 
-            
+      int[] arr= {10,20,30};
+      printArr(arr);
+      printArr(new int[] {40,50,60});
       
    }
+public static void printArr(int[] a) {
+   for (int i: a) {
+      System.out.println(i);
+   }
+}
 
+   
+   
 }
